0bfdb41de48479cf1eb53bdc4d305158c0cd6608
globefreak <globefreak@gmail.com>
2017-04-20 13:02:04 +0200

    first commit

---
+++ views/layouts/footer.php
@@ -0,0 +1,7 @@
+    <div class="container" id="footer">
+        <span style="float:left">
+            <i class="fa fa-copyright" aria-hidden="true"></i> 2017 <b>CWCLAN.DE</b> - clan &amp; community
+        </span>        
+        <span style="float:right">           
+        </span>   
+    </div>
---
+++ views/layouts/head.php
@@ -0,0 +1,19 @@
+<link href="<?= $this->theme->getBaseUrl(); ?>/css/theme.css" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl(); ?>/font/open_sans/open-sans.css" rel="stylesheet">
+
+<!-- start: Favicon and Touch Icons -->
+<link rel="apple-touch-icon" sizes="60x60" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-touch-icon-60x60-precomposed.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-touch-icon-76x76-precomposed.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-touch-icon-120x120-precomposed">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-touch-icon-152x152-precomposed.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-touch-icon-180x180-precomposed.png">
+<link rel="icon" type="image/png" sizes="192x192" href="<?= $this->theme->getBaseUrl(); ?>/ico/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= $this->theme->getBaseUrl(); ?>/ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?= $this->theme->getBaseUrl(); ?>/ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= $this->theme->getBaseUrl(); ?>/ico/favicon-16x16.png">
+<link rel="manifest" href="<?= $this->theme->getBaseUrl(); ?>/ico/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?= $this->theme->getBaseUrl(); ?>/ico/mstile-150x150.png">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
+<!-- end: Favicon and Touch Icons -->
